d705b70811683c13b44cd656c20a651ba8062e3b
Danilo Moura <danilogmoura@gmail.com>
2026-01-25 14:20:41 -0300

    Add LogManager class for structured logging functionality

---
+++ src/Utils/LogManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Firebot.Core;
+using MelonLoader;
+
+namespace Firebot.Utils;
+
+public class LogManager
+{
+    private const ConsoleColor TagColor = ConsoleColor.DarkCyan;
+    private const ConsoleColor TextColor = ConsoleColor.Gray;
+    private const ConsoleColor DebugColor = ConsoleColor.Blue;
+
+    public static void Info(string section, string message)
+    {
+        MelonLogger.Msg(TagColor, $"[{section}] ", TextColor, message);
+    }
+
+    public static void Warning(string section, string message)
+    {
+        MelonLogger.Warning($"[{section}] {message}");
+    }
+
+    public static void Error(string section, string message)
+    {
+        MelonLogger.Error($"[{section}] {message}");
+    }
+
+    public static void Debug(string section, string message)
+    {
+        if (BotSettings.DebugMode || MelonDebug.IsEnabled())
+            MelonLogger.Msg(TagColor, $"[{section}] ", DebugColor, $"[DEBUG] {message}");
+    }
+}
