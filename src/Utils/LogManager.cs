e50a50246930b219c4618353772437834292908a
Danilo Moura <danilogmoura@gmail.com>
2026-01-25 14:51:27 -0300

    Refactor LogManager to use static class; streamline logging methods and improve message formatting

--- src/Utils/LogManager.cs
+++ src/Utils/LogManager.cs
@@ -4,15 +4,16 @@ using MelonLoader;
 
 namespace Firebot.Utils;
 
-public class LogManager
+public static class LogManager
 {
     private const ConsoleColor TagColor = ConsoleColor.DarkCyan;
     private const ConsoleColor TextColor = ConsoleColor.Gray;
+    private const ConsoleColor ErrorColor = ConsoleColor.Red;
     private const ConsoleColor DebugColor = ConsoleColor.Blue;
 
     public static void Info(string section, string message)
     {
-        MelonLogger.Msg(TagColor, $"[{section}] ", TextColor, message);
+        MelonLogger.Msg(TextColor, $"[{section}] {message}");
     }
 
     public static void Warning(string section, string message)
@@ -28,6 +29,6 @@ public class LogManager
     public static void Debug(string section, string message)
     {
         if (BotSettings.DebugMode || MelonDebug.IsEnabled())
-            MelonLogger.Msg(TagColor, $"[{section}] ", DebugColor, $"[DEBUG] {message}");
+            MelonLogger.Msg(DebugColor, $"[DEBUG] [{section}] {message}");
     }
 }
